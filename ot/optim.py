7b3e134d039152207579c031be8f3ab1e6bccf8f
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2021-12-12 21:29:30 -0500

    update output rules

--- ot/optim.py
+++ ot/optim.py
@@ -1,9 +1,13 @@
 import torch
+import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def regularized_sinkhorn_knapp(r, c, M, reg, pre_P=None, stop_th=1e-9, check_freq=100, max_iters=1000, show_iter_error=True):
+def regularized_sinkhorn_knapp(r, c, M, reg, pre_P=None, stop_th=1e-9, check_freq=100, max_iters=1000, show_iter_info=True):
     
+    if show_iter_info:
+        start_time = time.time()
+        curr_time = time.time()
     r = torch.tensor(r, dtype=torch.double, device=device)
     c = torch.tensor(c, dtype=torch.double, device=device)
     M = torch.tensor(M, dtype=torch.double, device=device)
@@ -19,7 +23,11 @@ def regularized_sinkhorn_knapp(r, c, M, reg, pre_P=None, stop_th=1e-9, check_fre
         if iters % check_freq == 0:
             P = torch.matmul(torch.matmul(torch.diag(u), K), torch.diag(v))
             error = torch.sum(torch.abs(torch.sum(P, dim=0) - c)) + torch.sum(torch.abs(torch.sum(P, dim=1) - r))
-            print('iterations: ' + str(iters) + '; error : ' + str(error))
+            if show_iter_info:
+                iter_time = time.time() - curr_time
+                curr_time = time.time()
+                print('iters: ' + str(iters) + '; error : ' + "{0:.4g}".format(error.item()) \
+                      + "; elpse per check: " + "{0:.4g}".format(iter_time))
             if error < stop_th:
                 break
         if iters % 2 == 1:
@@ -32,5 +40,7 @@ def regularized_sinkhorn_knapp(r, c, M, reg, pre_P=None, stop_th=1e-9, check_fre
     P = torch.matmul(torch.matmul(torch.diag(u), K), torch.diag(v))
     P = torch.div(P, torch.sum(K))
     cost = torch.sum(P * M)
+    if show_iter_info:
+        print("total time: " + str(time.time() - start_time))
    
     return P, cost
